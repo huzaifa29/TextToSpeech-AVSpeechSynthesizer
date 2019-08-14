e226e7505fd6a6f7164dc6dff6638778ae1c3ac4
Huzaifa Ameen <huzaifa.ameen229@gmail.com>
2019-08-14 10:54:59 -0700

    Initial Commit

---
+++ TextToSpeech-AVSpeechSynthesizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextToSpeech-AVSpeechSynthesizer
+//
+//  Created by huzaifa on 8/14/19.
+//  Copyright © 2019 huzaifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
