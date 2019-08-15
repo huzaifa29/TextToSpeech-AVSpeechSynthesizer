01186ef134808522c6e1ea50195f02330f83db3d
Huzaifa Ameen <huzaifa.ameen229@gmail.com>
2019-08-15 14:06:53 -0700

    Initial Commit

--- TextToSpeech-AVSpeechSynthesizer/ViewController.swift
+++ TextToSpeech-AVSpeechSynthesizer/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
+    
+    
+    @IBOutlet var lblText: UILabel!
+    
+    var speechSynthesizer = AVSpeechSynthesizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        speechSynthesizer.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+        let mutableAttributedString = NSMutableAttributedString(string: utterance.speechString)
+        mutableAttributedString.addAttribute(.foregroundColor, value: UIColor.red, range: characterRange)
+        lblText.attributedText = mutableAttributedString
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        lblText.attributedText = NSAttributedString(string: utterance.speechString)
+    }
 
-
+    @IBAction func startPressed(_ sender: Any) {
+        let string = lblText.text
+        let utterance = AVSpeechUtterance(string: string!)
+//        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Ting-Ting-compact")
+        utterance.rate = 0.35
+        utterance.pitchMultiplier = 1.3
+        speechSynthesizer.speak(utterance)
+    }
+    
 }
 
